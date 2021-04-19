49fa65ac5ff8a39827cb7c160084b865fc672d04
Alice (VoevodovaNA) <voevodova02@mail.ru>
2021-04-19 16:21:24 +0300

    code: выделены функции

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ input_numbers(size_t count) {
 }
 
 void
-find_minmax(vector<double> numbers, double& min, double& max) {
+find_minmax(const vector<double>& numbers, double& min, double& max) {
     min = numbers[0];
     max = numbers[0];
     for (double number : numbers) {
@@ -26,21 +26,11 @@ find_minmax(vector<double> numbers, double& min, double& max) {
     }
 }
 
-int main() {
-
-    size_t number_count;
-    cout << "Enter number count: ";
-    cin >> number_count;
-    const auto numbers = input_numbers(number_count);
-
-    size_t bin_count;
-    cout << "Enter column count: ";
-    cin >> bin_count;
-
+vector<size_t>
+make_histogram(const vector<double> numbers, size_t bin_count) {
     double min, max;
-    find_minmax(numbers, min, max);
-
     vector<size_t> bins(bin_count);
+    find_minmax(numbers, min, max);
     for (double number : numbers) {
         size_t bin = (size_t)((number - min) / (max - min) * bin_count);
         if (bin == bin_count) {
@@ -48,8 +38,12 @@ int main() {
         }
         bins[bin]++;
     }
+ return bins;
+}
 
-
+void
+show_histogram_text(const vector<size_t>& bins)
+{
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
@@ -81,6 +75,26 @@ int main() {
         }
         cout << '\n';
     }
+}
+
+int main() {
+
+    size_t number_count;
+    cout << "Enter number count: ";
+    cin >> number_count;
+
+    const auto numbers = input_numbers(number_count);
+
+    size_t bin_count;
+    cout << "Enter column count: ";
+    cin >> bin_count;
+
+    double min, max;
+    find_minmax(numbers, min, max);
+
+    const auto bins = make_histogram(numbers, bin_count);
+
+    show_histogram_text(bins);
 
     return 0;
 }
