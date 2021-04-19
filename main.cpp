1a52816e13c0ce3d6592cabcd0544a7ce6d9d9d0
Alice (VoevodovaNA) <voevodova02@mail.ru>
2021-04-19 17:02:20 +0300

    code: начальная работа с гистограммой

--- main.cpp
+++ main.cpp
@@ -70,6 +70,8 @@ show_histogram_text(const vector<size_t>& bins)
             height = (size_t)(bin * scaling_factor);
         }
 
+
+
         for (size_t i = 0; i < height; i++) {
             cout << '*';
         }
@@ -77,16 +79,62 @@ show_histogram_text(const vector<size_t>& bins)
     }
 }
 
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void
+svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << baseline << "' y='35'>text</text>";
+}
+
+void svg_rect(double x, double y, double width, double height) {
+    cout << "<rect x='0' y='0' width='100' height='200' />";
+}
+
+void
+show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+        double top = 0;
+        for (size_t bin : bins) {
+            const double bin_width = BLOCK_WIDTH * bin;
+            svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+            svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+            top += BIN_HEIGHT;
+        }
+    svg_begin(400, 300);
+    svg_text(20, 20, to_string(bins[0]));
+    svg_rect(50, 0, bins[0] * 10, 30);
+    svg_end();
+}
+
 int main() {
 
     size_t number_count;
-    cout << "Enter number count: ";
+    cerr << "Enter number count: ";
     cin >> number_count;
 
     const auto numbers = input_numbers(number_count);
 
     size_t bin_count;
-    cout << "Enter column count: ";
+    cerr << "Enter column count: ";
     cin >> bin_count;
 
     double min, max;
@@ -94,7 +142,7 @@ int main() {
 
     const auto bins = make_histogram(numbers, bin_count);
 
-    show_histogram_text(bins);
+    show_histogram_svg(bins);
 
     return 0;
 }
