0c375cef5ef3a25e208ee370448660298ea59ba4
Alice (VoevodovaNA ) <voevodova02@mail.ru>
2021-05-17 19:58:04 +0300

    code: подключение файла histogram

---
+++ histogram.h
@@ -0,0 +1,26 @@
+#ifndef HISTOGRAM_H_INCLUDED
+#define HISTOGRAM_H_INCLUDED
+
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <string>
+
+
+using namespace std;
+vector<double> input_numbers(size_t count);
+void find_minmax(const vector<double>& numbers, double& min, double& max);
+vector<size_t> make_histogram(const vector<double> numbers, size_t bin_count);
+void show_histogram_text(vector<size_t>bins);
+void svg_begin(double width, double height);
+void svg_end();
+void svg_text(double left, double baseline, string text);
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black");
+void show_histogram_svg(const vector<size_t>& bins, char & sign);
+
+double fun_sign(const vector<double>& numbers,size_t bin_count);
+
+
+
+
+#endif // HISTOGRAM_H_INCLUDED
