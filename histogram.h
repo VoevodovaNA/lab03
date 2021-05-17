735884ff59223bb57ca7b822df6e85988c64b44e
Alice (VoevodovaNA ) <voevodova02@mail.ru>
2021-05-17 20:28:19 +0300

    code: подготовка к тестам

--- histogram.h
+++ histogram.h
@@ -1,26 +1,27 @@
-#ifndef HISTOGRAM_H_INCLUDED
-#define HISTOGRAM_H_INCLUDED
+//#ifndef HISTOGRAM_H_INCLUDED
+//#define HISTOGRAM_H_INCLUDED
 
+#pragma once
 #include <vector>
 #include <iostream>
 #include <math.h>
 #include <string>
+#include<iomanip>
 
 
 using namespace std;
 vector<double> input_numbers(size_t count);
 void find_minmax(const vector<double>& numbers, double& min, double& max);
-vector<size_t> make_histogram(const vector<double> numbers, size_t bin_count);
-void show_histogram_text(vector<size_t>bins);
+vector<double> make_histogram(const vector<double>& numbers, size_t bin_count);
+void show_histogram_text(vector<double>bins);
 void svg_begin(double width, double height);
 void svg_end();
 void svg_text(double left, double baseline, string text);
 void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black");
-void show_histogram_svg(const vector<size_t>& bins, char & sign);
-
-double fun_sign(const vector<double>& numbers,size_t bin_count);
-
+void show_histogram_svg(const vector<double>& bins, double val_sign);
+double fun_sign(const vector<double> numbers,size_t bin_count);
 
 
 
-#endif // HISTOGRAM_H_INCLUDED
+
+//#endif // HISTOGRAM_H_INCLUDED
