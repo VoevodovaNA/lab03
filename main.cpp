8f745f55c10a84cc480ed7bfa229bc7daec741f6
Alice (IvanovII) <alice@example.com>
2021-06-06 12:30:39 +0300

    не конец

--- main.cpp
+++ main.cpp
@@ -9,7 +9,10 @@
 using namespace std;
 using std::vector;
 
-vector<double> input_numbers(size_t count) {
+    const size_t SCREEN_WIDTH = 80;
+    const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+
+vector<double> input_numbers(double count) {
     vector<double> result(count);
     for (size_t i = 0; i < count; i++) {
         cin >> result[i];
@@ -32,53 +35,40 @@ make_histogram(const vector<double>& numbers, size_t bin_count) {
  return bins;
 }
 
-void
-show_histogram_text(const vector<size_t> bins)
+void show_histogram_text(const vector<double>& bins)
 {
-    int bin_count = bins.size();
-    int max_count = 0;
-    for (size_t i = 0; i < bin_count; i++)
-    {
-        size_t count = bins[i];
-
-        if (count > max_count)
+    double max_count = 0;
+    for (double count : bins)
         {
+        if (count > max_count)
             max_count = count;
         }
-    }
-    double height;
-    const size_t SCREEN_WIDTH = 80;
-    const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-    if (max_count > MAX_ASTERISK)
-    {
-        height = (static_cast<double>(MAX_ASTERISK) / max_count);;
-    }
-    else
-    {
-        height = 1;
-    }
-    for (size_t i = 0; i < bin_count; i++)
-    {
-        size_t k = bins[i];
-        cout << "   ";
-        if (k < 100)
-        {
-            cout << " ";
-        }
-        if (k < 10)
-        {
-            cout << " ";
-        }
-        cout << k << "|";
-        for (size_t j = 0; j < k * height; j++)
-        {
-            cout << "*";
-        }
-        cout << "\n";
 
+    const bool scaling_needed = max_count > MAX_ASTERISK;
 
+    for (double bin : bins)
+    {
+        if (bin < 100)
+            cout << ' ';
+
+        if (bin < 10)
+            cout << ' ';
+
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed)
+            {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+            }
+
+        for (size_t i = 0; i < height; i++)
+            cout << '*';
+
+        cout << '\n';
     }
-    return;
+
 }
 
 
@@ -91,8 +81,11 @@ int main() {
     size_t bin_count;
     cerr << "Enter column count: ";
     cin >> bin_count;
-    //double min, max;
-    //find_minmax(numbers, min, max);
+    /*double min, max;
+    find_minmax(numbers, min, max);
+     const auto bins = make_histogram(numbers, min, max, bin_count);
+    show_histogram_svg(bins,stroke,fill);
+    */
     const auto bins = make_histogram(numbers, bin_count);
     auto val_sign = fun_sign(numbers, bin_count);
     show_histogram_svg(bins,val_sign);
