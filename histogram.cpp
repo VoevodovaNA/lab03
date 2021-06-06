ca6983d9aea58329957188be8d58a190746cdf54
VoevodovaNA <voevodova02@mail.ru>
2021-06-06 22:55:09 +0300

    4lab

--- histogram.cpp
+++ histogram.cpp
@@ -35,3 +35,54 @@ double mean(const vector<size_t>& bins) {
     result=sum/((double)(bins.size()));
     return result;
 }
+
+vector<size_t> make_histogram(const Input& data)
+{
+    double min, max;
+    find_minmax(data.numbers, min, max);
+    vector<size_t> result(data.bin_count);
+    for (double number : data.numbers) {
+        size_t bin = (size_t)((number - min) / (max - min) * data.bin_count);
+        if (bin == data.bin_count) {
+            bin--;
+        }
+        result[bin]++;
+    }
+    return result;
+}
+
+void show_histogram_text(const vector<size_t>& bins)
+{
+    size_t max_count = 0;
+    for (size_t count : bins)
+        {
+        if (count > max_count)
+            max_count = count;
+        }
+
+    const bool scaling_needed = max_count > MAX_ASTERISK;
+
+    for (size_t bin : bins)
+    {
+        if (bin < 100)
+            cout << ' ';
+
+        if (bin < 10)
+            cout << ' ';
+
+        cout << bin << "|";
+
+        size_t height = bin;
+        if (scaling_needed)
+            {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            height = (size_t)(bin * scaling_factor);
+            }
+
+        for (size_t i = 0; i < height; i++)
+            cout << '*';
+
+        cout << '\n';
+    }
+
+}
