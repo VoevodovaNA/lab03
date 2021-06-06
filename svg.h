c2c29c566ab9649706c4172aef910addc9a2add5
Alice (IvanovII) <alice@example.com>
2021-06-06 12:30:05 +0300

    не конец

--- svg.h
+++ svg.h
@@ -8,7 +8,7 @@ using namespace std;
 void svg_begin(double width, double height);
 void svg_end();
 void svg_text(double left, double baseline, string text);
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black");
-double fun_sign(const vector<double> numbers,size_t bin_count);
-void show_histogram_svg(const vector<double>& bins, double val_sign);
+void svg_rect(double x, double y, double width, double height, string stroke, string fill  );
+double fun_sign(const vector<double> numbers,size_t bin_count );
+void show_histogram_svg(const vector<double>& bins,double val_sign);
 #endif // SVG_H_INCLUDED
